6ae9bcad778607086c39cca6f71e19eff9cf6d54
naveenclgit <61846339+naveenclgit@users.noreply.github.com>
2024-01-06 17:58:16 -0800

    Update Hello.java for Build 1

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
      public static void main(String[] args) {
      System.out.println("Welcome to Jenkins's Online Training!");
      System.out.println("This is for Jenkins");
+     System.out.println("This is for Build1");
+          
          }
 }
